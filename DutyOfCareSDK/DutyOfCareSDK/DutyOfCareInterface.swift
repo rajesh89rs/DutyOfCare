432f870f9d66f4cc1681a8863fb9aec24f6c56d1
rajesh <rajesh.ramshettysiddaraju@amadeus.com>
2018-12-06 14:57:56 +0530

    initial commit

---
+++ DutyOfCareSDK/DutyOfCareSDK/DutyOfCareInterface.swift
@@ -0,0 +1,18 @@
+//
+//  DutyOfCareInterface.swift
+//  DutyOfCareSDK
+//
+//  Created by Rajesh RAMSHETTY SIDDARAJU on 06/12/18.
+//  Copyright © 2018 cytric. All rights reserved.
+//
+
+import Foundation
+
+@objc public class DutyOfCareInterface: NSObject {
+    
+    public func testDOCInterface() -> Bool {
+        print("#############DutyOfCareInterface#############")
+        return true
+    }
+    
+}
