c76f6828afb7bf22da8bb68d3b7820aae9075b6b
rajesh <rajesh.ramshettysiddaraju@amadeus.com>
2018-12-06 15:44:44 +0530

    added framework and spec  file

---
+++ DutyOfCareSDK.framework/Headers/DutyOfCareSDK.h
@@ -0,0 +1,19 @@
+//
+//  DutyOfCareSDK.h
+//  DutyOfCareSDK
+//
+//  Created by Rajesh RAMSHETTY SIDDARAJU on 06/12/18.
+//  Copyright © 2018 cytric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DutyOfCareSDK.
+FOUNDATION_EXPORT double DutyOfCareSDKVersionNumber;
+
+//! Project version string for DutyOfCareSDK.
+FOUNDATION_EXPORT const unsigned char DutyOfCareSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DutyOfCareSDK/PublicHeader.h>
+
+
